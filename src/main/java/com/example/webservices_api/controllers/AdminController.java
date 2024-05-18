540abf61947bf8a03345d6fbda4d1f4075c0fd6c
Petter Lindh <petter.s.lindh@gmail.com>
2024-05-18 17:36:11 +0200

    2nd commit. @RequestMapping for shorter endpoints.

--- src/main/java/com/example/webservices_api/controllers/AdminController.java
+++ src/main/java/com/example/webservices_api/controllers/AdminController.java
@@ -11,34 +11,35 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
     @Autowired
     private ProductService productService;
 
     // Get all products
-    @GetMapping("/admin/products")
+    @GetMapping("/products")
     @ResponseBody
     public ResponseEntity<List<Product>> getAllProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
     // Create a new product
-    @PostMapping("admin/addproduct")
+    @PostMapping("addproduct")
     @ResponseBody
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
     }
 
     // Delete a product
-    @DeleteMapping("admin/deleteproduct/{id}")
+    @DeleteMapping("deleteproduct/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable("id") int id) {
         productService.deleteProductById(id);
         return ResponseEntity.ok("Product with id " +id + " was deleted");
     }
 
     // Update a product
-    @PutMapping("admin/updateproduct/{id}")
+    @PutMapping("updateproduct/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
         return ResponseEntity.ok(productService.updateProduct(id, product));
     }
--- src/main/java/com/example/webservices_api/controllers/UserController.java
+++ src/main/java/com/example/webservices_api/controllers/UserController.java
@@ -6,18 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private ProductService productService;
 
     // Get all products
-    @GetMapping("/user/products")
+    @GetMapping("/products")
     @ResponseBody
     public ResponseEntity<List<Product>> getAllProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
